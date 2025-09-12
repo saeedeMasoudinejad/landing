cd3f8359d56cbcba2444105e368edccc821fc9b2
saeedeMasoudinejad <58415361+saeedeMasoudinejad@users.noreply.github.com>
2025-09-12 17:56:17 +0330

    Refactor render

--- src/scripts/render.js
+++ src/scripts/render.js
@@ -1,30 +1,18 @@
+const priorityEnum = {
+    low: 'پایین', medium: 'متوسط', high: 'بالا'
+}
+const priorityStyle = {
+    low: {bg: "bg-green-100", text: "text-green-700", bgLabel:"bg-green-700"},
+    medium: {bg: "bg-orange-100", text: "text-orange-500", bgLabel:"bg-orange-500"},
+    high: {bg: "bg-red-100", text: "text-red-500", bgLabel:"bg-red-500"},
+};
 
-// function generateHtmlTask(Task):
-
-
-export function Render() {
-    const priorityEnum = {
-        low: 'پایین',
-        medium: 'متوسط',
-        high: 'بالا'
-    }
-    const priorityStyle = {
-        low: {bg: "bg-green-100", text: "text-green-600"},
-        medium: {bg: "bg-yellow-100", text: "text-yellow-600"},
-        high: {bg: "bg-red-100", text: "text-red-600"},
-    };
-    const tasks = [
-        {id: 1, title: 'Task 1', description: 'Task 1 description', priority: 'low', isDone: false, isEditing: false},
-        {id: 2, title: 'SUbTask 2', description: 'Task 2 description', priority: 'medium', isDone: false, isEditing: true},
-        {id: 3, title: 'SUbTask 3', description: 'Task 3 dddddddescription', priority: 'low', isDone: true, isEditing: true},
-        {id: 4, title: 'Task 4', description: 'Task 4 ', priority: 'high', isDone: false, isEditing: true},
-        {id: 5, title: 'Task 5', description: 'Task 55555555 ', priority: 'high', isDone: true, isEditing: true},
-
-    ]
-    const InProgressTask = document.getElementById('in-progress-tasks-list');
-    InProgressTask.innerHTML = tasks.map((task)=>`
+function generateHtmlTask(task) {
+    console.log(priorityStyle[task.priority]);
+    console.log(task.priority);
+    const taskHtml = `
         <div class="task border border-primary-oil-04 rounded p-4 relative flex items-start justify-between w-full  mx-auto mb-4">
-            <div class="absolute top-0 right-0  w-1 rounded-tl rounded-bl h-[76px] my-4 lg:my-2  item-center bg-${priorityStyle[task.priority]}"></div>
+            <div class="absolute top-0 right-0  w-1 rounded-tl rounded-bl h-[76px] my-4 lg:my-2  item-center ${priorityStyle[task.priority].bgLabel}"></div>
             <!-- Right section: Task info + checkbox -->
             <div class="flex items-start gap-2">
                 <!-- Checkbox -->
@@ -35,7 +23,7 @@ export function Render() {
                 <div>
                     <div id="task-title-1" class="flex flex-col lg:flex-row items-start  lg:gap-4" >
                         <h3 class="text-4 font-bold text-primary-oil-10">${task.title}</h3>
-                        <span class="text-[10px] font-semibold bg-red-100 text-red-600 rounded-[2px] px-2 py-0.5 mb-4 mt-1 lg:mb-[110x]">${priorityEnum[task.priority]}</span>
+                        <span class="text-[10px] font-semibold ${priorityStyle[task.priority].bg} ${priorityStyle[task.priority].text} rounded-[2px] px-2 py-0.5 mb-4 mt-1 lg:mb-[110x]">${priorityEnum[task.priority]}</span>
                     </div>
                     <p class="text-[12px] text-primary-neutral-700">${task.description}</p>
                 </div>
@@ -49,6 +37,29 @@ export function Render() {
                 </button>
             </div>
         </div>
-    `).join("");
+    `
+    return taskHtml;
+}
+
+
+export function Render(tasks) {
+
+
+    // console.log(tasks);
+    let isDoneTaskList = [];
+    let inProgressTaskList = [];
+    const inProgressTaskContainer = document.getElementById('in-progress-tasks-list');
+    const isDoneTaskContainer = document.getElementById('is-done-tasks-list');
+    tasks.map((task) => {
+        if (task.isDone) isDoneTaskList.push(task);
+        else inProgressTaskList.push(task);
+    });
+    if (inProgressTaskList.length !== 0) {
+        document.getElementById('add-task-form').nextElementSibling.style.display = 'none';
+    }
+    inProgressTaskContainer.innerHTML = inProgressTaskList.sort((a, b) => a.id - a.id).map((task) => generateHtmlTask(task)).join("");
+    // console.log(inProgressTaskList);
+    isDoneTaskContainer.innerHTML = isDoneTaskList.sort((a, b) => a.id - a.id).map((task) => generateHtmlTask(task)).join("");
+    // console.log(isDoneTaskList);
 
 }
