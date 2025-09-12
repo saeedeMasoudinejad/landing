05764360d1cf80f37969be829395399bcdd8b28c
saeedeMasoudinejad <58415361+saeedeMasoudinejad@users.noreply.github.com>
2025-09-12 15:42:56 +0330

    Call render function.

--- src/main.js
+++ src/main.js
@@ -12,9 +12,12 @@
 
 
 
+
+
 console.log(document.body.dataset)
 import { addTask } from "./scripts/add-task.js";
 import { taskFormToggle } from "./scripts/task-form-toggle.js";
+import {Render} from "./scripts/render.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     /* ------------------------- about showing task form ------------------------ */
@@ -22,6 +25,7 @@ document.addEventListener("DOMContentLoaded", () => {
     /* -------------------------- about rendering tasks ------------------------- */
     let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
     //renderTasks(tasks);
+    Render()
     const submitTaskButton = document.getElementById("submit-add-task");
     submitTaskButton.addEventListener("click", (e) => {
         e.preventDefault();
