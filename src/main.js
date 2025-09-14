26e91699082f3a250640e2944096fc0917e03b70
mohammad <mohammadariani97@gmail.com>
2025-09-14 06:52:38 +0300

    final edit

--- src/main.js
+++ src/main.js
@@ -1,36 +1,3 @@
-
-
-const themeToggle = document.getElementById("theme-toggle");
-const htmlElement = document.documentElement;
-const modeIcon = document.getElementById("mode-icon");
-
-// بررسی حالت فعلی (برای حفظ حالت در بارگذاری مجدد)
-if (
-  localStorage.getItem("theme") === "dark" ||
-  (!("theme" in localStorage) &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches)
-) {
-  htmlElement.classList.add("dark");
-  modeIcon.src = "src/assets/icons/light_icon.png";
-} else {
-  htmlElement.classList.remove("dark");
-  modeIcon.src = "src/assets/icons/dark_icon.png";
-}
-
-themeToggle.addEventListener("click", () => {
-  // تغییر کلاس dark
-  htmlElement.classList.toggle("dark");
-
-  // تغییر آیکون بر اساس حالت جدید
-  if (htmlElement.classList.contains("dark")) {
-    modeIcon.src = "src/assets/icons/light_icon.png";
-    localStorage.setItem("theme", "dark");
-  } else {
-    modeIcon.src = "src/assets/icons/dark_icon.png";
-    localStorage.setItem("theme", "light");
-  }
-});
-
 console.log(document.body.dataset);
 import { addTask } from "./scripts/add-task.js";
 import { taskFormToggle } from "./scripts/task-form-toggle.js";
